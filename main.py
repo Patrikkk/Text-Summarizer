b415c61f256c463c8e98b14e009a1932813de4ac
Patrikkk <patrikveres@hotmail.com>
2023-01-06 00:26:31 +0100

    Modify some code to allow public access.

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def summary():
 
 if __name__ == '__main__':
     # app.before_request(before_request)
-    app.jinja_env.auto_reload = True
+    # app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
